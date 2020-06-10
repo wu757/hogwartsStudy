c008568ab52350409416f27938f7c86361691266
wu757 <604254405@qq.com>
2020-06-11 00:35:56 +0800

    提交作业一

---
+++ python实战一/pythoncode/module/func.py
@@ -0,0 +1,19 @@
+def have_parameter(para):
+    print(f"输出参数: {para}")
+
+
+def no_parameter():
+    print("无参数")
+
+
+def have_return(value):
+    print("这函数有返回值: ")
+    return value
+
+
+def no_retrun():
+    print("无return时默认返回：None")
+
+
+if __name__ == '__main__':
+    no_parameter()

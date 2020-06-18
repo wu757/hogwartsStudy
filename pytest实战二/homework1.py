6e800747726a3726f584045fb2098c4de54c5d7e
wu757 <604254405@qq.com>
2020-06-18 21:47:20 +0800

    用例上下的依赖和执行顺序

--- pytest实战二/homework1.py
+++ pytest实战二/homework1.py
@@ -5,34 +5,46 @@ with open("./data_cal.yaml",'rb') as f:
     test_data=yaml.safe_load(f)
 
 class TestCal():
+    @pytest.mark.dependency()
+    @pytest.mark.run(order=1)
     @pytest.mark.parametrize("a,b,expect",test_data["add"])
     def test_add(self,a,b,expect,init_cal):
-        assert init_cal.add(a,b)==expect
+        assert init_cal.add(a,b)==111
 
+    @pytest.mark.dependency(depends=["test_add"])
+    @pytest.mark.run(order=2)
     @pytest.mark.parametrize("a,b,expect", test_data["sub"])
     def test_sub(self,a,b,expect,init_cal):
         assert init_cal.sub(a,b)==expect
 
+    @pytest.mark.dependency()
+    @pytest.mark.run(order=3)
     @pytest.mark.parametrize("a,b,expect", test_data["mul"])
     def test_mul(self,a,b,expect,init_cal):
         assert init_cal.mul(a,b)==expect
 
+    @pytest.mark.dependency(depends=["test_mul"])
+    @pytest.mark.run(order=4)
     @pytest.mark.parametrize("a,b,expect", test_data["div"])
     def test_div(self, a, b, expect, init_cal):
         assert init_cal.div(a, b) == expect
 
+    @pytest.mark.run(order=1)
     @pytest.mark.parametrize("a,b,expect", test_data["add"])
     def check_add(self, a, b, expect, init_cal):
         assert init_cal.add(a, b) == expect
 
+    @pytest.mark.run(order=2)
     @pytest.mark.parametrize("a,b,expect", test_data["sub"])
     def check_sub(self,a,b,expect,init_cal):
         assert init_cal.sub(a,b)==expect
 
+    @pytest.mark.run(order=3)
     @pytest.mark.parametrize("a,b,expect", test_data["mul"])
     def check_mul(self,a,b,expect,init_cal):
         assert init_cal.mul(a,b)==expect
 
+    @pytest.mark.run(order=4)
     @pytest.mark.parametrize("a,b,expect", test_data["div"])
     def check_div(self, a, b, expect, init_cal):
         assert init_cal.div(a, b) == expect
